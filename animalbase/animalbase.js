928c452752b0c87eb73b8bfe05703842fde17c59
Louise <lodo0001@stud.kea.dk>
2026-02-10 10:40:15 +0100

    dragons

--- animalbase/animalbase.js
+++ animalbase/animalbase.js
@@ -4,71 +4,120 @@ window.addEventListener("DOMContentLoaded", start);
 
 let allAnimals = [];
 
-// The prototype for all animals: 
+// The prototype for all animals:
 const Animal = {
-    name: "",
-    desc: "-unknown animal-",
-    type: "",
-    age: 0
+  name: "",
+  desc: "-unknown animal-",
+  type: "",
+  age: 0,
 };
 
-function start( ) {
-    console.log("ready");
+function start() {
+  console.log("ready");
 
-    // TODO: Add event-listeners to filter and sort buttons
-
-    loadJSON();
+  // TODO: Add event-listeners to filter and sort buttons
+  registerButtons();
+  loadJSON();
 }
 
+// vi laver en funktion til knapperne
+function registerButtons() {
+  document
+    .querySelectorAll("[data-action='filter']")
+    .forEach((button) => button.addEventListener("click", selectFilter));
+}
 
 async function loadJSON() {
-    const response = await fetch("animals.json");
-    const jsonData = await response.json();
-    
-    // when loaded, prepare data objects
-    prepareObjects( jsonData );
+  const response = await fetch("animals.json");
+  const jsonData = await response.json();
+
+  // when loaded, prepare data objects
+  prepareObjects(jsonData);
 }
 
-function prepareObjects( jsonData ) {
-    allAnimals = jsonData.map( preapareObject );
+function prepareObjects(jsonData) {
+  allAnimals = jsonData.map(preapareObject);
 
-    // TODO: This might not be the function we want to call first
-    displayList(allAnimals);
+  // TODO: This might not be the function we want to call first
+  displayList(allAnimals);
 }
 
-function preapareObject( jsonObject ) {
-    const animal = Object.create(Animal);
-    
-    const texts = jsonObject.fullname.split(" ");
-    animal.name = texts[0];
-    animal.desc = texts[2];
-    animal.type = texts[3];
-    animal.age = jsonObject.age;
+function preapareObject(jsonObject) {
+  const animal = Object.create(Animal);
 
-    return animal;
+  const texts = jsonObject.fullname.split(" ");
+  animal.name = texts[0];
+  animal.desc = texts[2];
+  animal.type = texts[3];
+  animal.age = jsonObject.age;
+
+  return animal;
 }
 
+function selectFilter(event) {
+  const filter = event.target.dataset.filter;
+  console.log(`User selected ${filter}`);
+  filterList(filter);
+}
+
+function filterList(animalType) {
+  // Hvis det ikke enten er kat eller hund, så vises alle dyr
+  let filteredList = allAnimals;
+  if (animalType === "cat") {
+    // Vi laver en filtering med en liste af kun katte.
+    filteredList = allAnimals.filter(isCat);
+  } else if (animalType === "dog") {
+    // Vi laver en filtering med en liste af kun hunde.
+    filteredList = allAnimals.filter(isDog);
+  } else if (animalType === "horse") {
+    filteredList = allAnimals.filter(isHorse);
+  } else if (animalType === "dragon") {
+    filteredList = allAnimals.filter(isDragon);
+  }
+
+  displayList(filteredList);
+
+  function isCat(animal) {
+    // if (animal.type === "cat") {
+    //   return true;
+    // } else {
+    //   return false;
+    // }
+    // I stedet for at skrive if-sætning, kan vi skrive..
+    return animal.type === "cat";
+  }
+
+  function isDog(animal) {
+    return animal.type === "dog";
+  }
+  function isHorse(animal) {
+    return animal.type === "horse";
+  }
+  function isDragon(animal) {
+    return animal.type === "dragon";
+  }
+}
 
 function displayList(animals) {
-    // clear the list
-    document.querySelector("#list tbody").innerHTML = "";
+  // clear the list
+  document.querySelector("#list tbody").innerHTML = "";
 
-    // build a new list
-    animals.forEach( displayAnimal );
+  // build a new list
+  animals.forEach(displayAnimal);
 }
 
-function displayAnimal( animal ) {
-    // create clone
-    const clone = document.querySelector("template#animal").content.cloneNode(true);
+function displayAnimal(animal) {
+  // create clone
+  const clone = document
+    .querySelector("template#animal")
+    .content.cloneNode(true);
 
-    // set clone data
-    clone.querySelector("[data-field=name]").textContent = animal.name;
-    clone.querySelector("[data-field=desc]").textContent = animal.desc;
-    clone.querySelector("[data-field=type]").textContent = animal.type;
-    clone.querySelector("[data-field=age]").textContent = animal.age;
+  // set clone data
+  clone.querySelector("[data-field=name]").textContent = animal.name;
+  clone.querySelector("[data-field=desc]").textContent = animal.desc;
+  clone.querySelector("[data-field=type]").textContent = animal.type;
+  clone.querySelector("[data-field=age]").textContent = animal.age;
 
-    // append clone to list
-    document.querySelector("#list tbody").appendChild( clone );
+  // append clone to list
+  document.querySelector("#list tbody").appendChild(clone);
 }
-
-
